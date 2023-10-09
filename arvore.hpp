5c57fee4faa188f36660f1f49c698f9ab74f3586
Italo Portinho <italons@gmail.com>
2023-10-09 14:53:42 -0300

    Resolução da questão 3.

---
+++ arvore.hpp
@@ -0,0 +1,149 @@
+// Copyright (C) 2023 - Italo Portinho
+#include <cstdio>
+#include <algorithm>
+class NoArvore {
+ public:
+    int dado;
+    NoArvore* esq;
+    NoArvore* dir;
+};
+
+class Arvore {
+ public:
+    NoArvore* raiz;
+};
+
+//-------------------------------------------------------------------
+//  a)      Computar a soma das folhas
+//-------------------------------------------------------------------
+int soma_folhas(NoArvore* no, int soma) {
+    if (no->esq == 0 && no->dir == 0) {
+        printf("Folha = %d \n", no->dado);
+        printf("Soma era = %d \n", soma);
+        soma = soma + no->dado;
+        printf("Soma agora e = %d \n", soma);
+    } else {
+        soma = soma_folhas(no->esq, soma);
+        soma = soma_folhas(no->dir, soma);
+    }
+    return soma;
+}
+
+void soma_folhas_alt(NoArvore* no, int &soma) {
+    if (no->esq == 0 && no->dir == 0) {
+        printf("Folha = %d \n", no->dado);
+        printf("Soma era = %d \n", soma);
+        soma = soma + no->dado;
+        printf("Soma agora e = %d \n", soma);
+    } else {
+        if (no->esq != 0) {
+            soma_folhas_alt(no->esq, soma);
+        }
+        if (no->dir != 0) {
+            soma_folhas_alt(no->dir, soma);
+        }
+    }
+}
+
+//-------------------------------------------------------------------
+//  b)      Percurso em pós-ordem (esq, dir, nó)
+//-------------------------------------------------------------------
+void pos_ordem(NoArvore* no) {
+    if (no->esq) {
+        pos_ordem(no->esq);
+    }
+    if (no->dir) {
+        pos_ordem(no->dir);
+    }
+    printf("%d ", no->dado);
+    return;
+}
+
+//-------------------------------------------------------------------
+//  c)      Percurso em-ordem (esq, no, dir)
+//-------------------------------------------------------------------
+void em_ordem(NoArvore* no) {
+    if (no->esq) {
+        em_ordem(no->esq);
+    }
+    printf("%d ", no->dado);
+    if (no->dir) {
+        em_ordem(no->dir);
+    }
+    return;
+}
+
+//-------------------------------------------------------------------
+//  d)      Percurso em pré-ordem (no, esq, dir)
+//-------------------------------------------------------------------
+void pre_ordem(NoArvore* no) {
+    printf("%d ", no->dado);
+    if (no->esq) {
+        pre_ordem(no->esq);
+    }
+    if (no->dir) {
+        pre_ordem(no->dir);
+    }
+    return;
+}
+
+//-------------------------------------------------------------------------------
+//  e)      Calcular a altura de um nó (o maior caminho de um nó até uma folha)
+//-------------------------------------------------------------------------------
+int calculaAltura(NoArvore* no, int altura) {
+    if (!no) {
+        return 0;
+    }
+    int altura_esq = calculaAltura(no->esq, altura);
+    int altura_dir = calculaAltura(no->dir, altura);
+    int max_altura = std::max(altura_esq, altura_dir) + 1;
+
+    return max_altura;
+}
+
+//-------------------------------------------------------------------
+//  f)      Fator de balanceamento de um nó (O balanceamento de um NÓ é definido
+//  como a altura de sua subárvore esquerda menos a altura de sua subárvore
+//  direita - Fonte: USP)
+//-------------------------------------------------------------------
+int balanceamento_no(NoArvore* no) {
+    int altura_sub_esquerda = calculaAltura(no->esq, 0);
+    int altura_sub_direita = calculaAltura(no->dir, 0);
+    return altura_sub_esquerda - altura_sub_direita;
+}
+
+//--------------------------------------------------------------------------
+//  g)      Percorrer a árvore em níveis (representar a árvore em um vetor)
+//--------------------------------------------------------------------------
+void print_nivel(NoArvore* no, int nivel) {
+    if (!no)
+        return;
+    if (nivel == 1) {
+        printf(" %d ", no->dado);
+    } else if (nivel > 1) {
+        print_nivel(no->esq, nivel - 1);
+        print_nivel(no->dir, nivel - 1);
+    }
+    return;
+}
+void percorre_niveis(NoArvore* no) {
+    int altura = calculaAltura(no, 0);
+    for (int i = 1; i <= altura; i++) {
+        print_nivel(no, i);
+    }
+}
+
+
+//-------------------------------------------------------------------
+//  h)      Produto de todos os nós
+//-------------------------------------------------------------------
+int produto_nos(NoArvore* no, int produto) {
+    produto = produto * no->dado;
+    if (no->esq) {
+        produto = produto_nos(no->esq, produto);
+    }
+    if (no->dir) {
+        produto = produto_nos(no->dir, produto);
+    }
+    return produto;
+}
---
+++ main.cpp
@@ -0,0 +1,36 @@
+// Copyright (C) 2023 - Italo Portinho
+// Para compilar(windows): g++ main.cpp -o main
+#include <cstdio>
+#include "./arvore.hpp"
+
+int main() {
+    int const MAX_N = 9;
+    int vetor[MAX_N] = { 10, 20, 15, 12, 8, 5, 7, 1, 2 };
+
+    Arvore minha_arvore = {};
+    NoArvore* level_4_2 = new NoArvore { .dado = vetor[8], .esq = 0, .dir = 0 };
+    NoArvore* level_4_1 = new NoArvore { .dado = vetor[7], .esq = 0, .dir = 0 };
+    NoArvore* level_3_4 = new NoArvore { .dado = vetor[6], .esq = 0, .dir = 0 };
+    NoArvore* level_3_3 = new NoArvore { .dado = vetor[5], .esq = 0, .dir = 0 };
+    NoArvore* level_3_2 = new NoArvore { .dado = vetor[4], .esq = 0, .dir = 0 };
+    NoArvore* level_3_1 = new NoArvore { .dado = vetor[3], .esq = level_4_1, .dir = level_4_2 };
+    NoArvore* level_2_2 = new NoArvore { .dado = vetor[2], .esq = level_3_3, .dir = level_3_4 };
+    NoArvore* level_2_1 = new NoArvore { .dado = vetor[1], .esq = level_3_1, .dir = level_3_2 };
+    NoArvore* level_1_1 = new NoArvore { .dado = vetor[0], .esq = level_2_1, .dir = level_2_2 };
+    minha_arvore.raiz = level_1_1;
+/*
+    printf("Soma folhas = %d \n", soma_folhas(minha_arvore.raiz, 0));
+    printf("\nPos-ordem = \n");
+    pos_ordem(minha_arvore.raiz);
+    printf("\nEm-ordem = \n");
+    em_ordem(minha_arvore.raiz);
+    printf("\nPre-ordem = \n");
+    pre_ordem(minha_arvore.raiz);
+    
+    printf("altura no 20: %d\n", calculaAltura(level_3_2, 0));       
+    percorre_niveis(minha_arvore.raiz);
+    printf("Produto == %d ", produto_nos(minha_arvore.raiz, 1));
+
+    */
+    printf("Balanceamento == %d ", balanceamento_no(level_2_1));
+}
